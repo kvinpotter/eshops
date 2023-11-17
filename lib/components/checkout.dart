9071e41fd2277164d7a77eb0b76b45b6dfb70409
kvinpotter <43843470+kvinpotter@users.noreply.github.com>
2023-11-17 17:08:29 +0300

    Update checkout.dart

--- lib/components/checkout.dart
+++ lib/components/checkout.dart
@@ -89,7 +89,7 @@ class CheckoutPage extends StatelessWidget {
 
                     // Send the order information to the PHP script
                     var response = await http.post(
-                      Uri.parse('https://your_server.com/place_order.php'),
+                      Uri.parse('https://127.0.0.1:3306/place_order.php'),
                       body: {
                         'userName': userName,
                         'userAddress': userAddress,
