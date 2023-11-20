6793cd15f93364d4dad890c3ade62250011897f7
kelvin <kelvinmassai@amplepack.co.tz>
2023-11-20 14:10:11 +0300

    new changes

--- lib/php APi/order.php
+++ lib/php APi/order.php
@@ -1,6 +1,25 @@
 <?php
 
-$servername = "localhost";
+// order.php
+
+// Set the timezone (replace 'your_timezone' with a valid timezone identifier)
+date_default_timezone_set('UTC');
+
+// Receive data from Flutter app
+$input_data = file_get_contents('php://input');
+$data = json_decode($input_data, true);
+
+// Debugging: Log the received data
+error_log("Received data: " . print_r($data, true));
+
+// Extract data
+$userName = isset($data['userName']) ? $data['userName'] : '';
+$userAddress = isset($data['userAddress']) ? $data['userAddress'] : '';
+$paymentDetails = isset($data['paymentDetails']) ? $data['paymentDetails'] : '';
+$orderItems = isset($data['orderItems']) ? $data['orderItems'] : '';
+
+// Insert data into the database
+$servername = "127.0.0.1";
 $username = "root";
 $password = "";
 $dbname = "ellipsis_db";
@@ -11,22 +30,21 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Get data from POST request
-$userName = $_POST['userName'];
-$userAddress = $_POST['userAddress'];
-$paymentDetails = $_POST['paymentDetails'];
-$orderItems = $_POST['orderItems'];
+// Get the current timestamp
+$orderDate = date('Y-m-d H:i:s');
 
-// Insert user details into 'order_information' table
-$sql = "INSERT INTO order_information (user, address, paymentDetails, orderItems, orderDate) 
-        VALUES ('$userName', '$userAddress', '$paymentDetails', '$orderItems', NOW())";
+// Prepare and execute SQL query to insert data
+$sql = "INSERT INTO order_information (user, address, paymentDetails, orderDate, orderItems) VALUES ('$userName', '$userAddress', '$paymentDetails', '$orderDate', '$orderItems')";
 
 if ($conn->query($sql) === TRUE) {
-    echo "Order placed successfully";
+    // Database insertion successful
+    echo json_encode(['message' => 'Order placed successfully']);
 } else {
-    echo "Error placing order: " . $conn->error;
+    // Error in database insertion
+    echo json_encode(['error' => 'Error placing order: ' . $conn->error]);
 }
 
+// Close the database connection
 $conn->close();
 
 ?>
